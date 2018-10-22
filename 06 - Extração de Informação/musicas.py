deaed9633c1fc7c8467bdc1eaf5d9043a017ed5f
Matias159 <alexandrehmatias@hotmail.com>
2018-10-22 20:34:15 -0200

    extração de musicas concluida

--- 06 - Extração de Informação/musicas.py
+++ 06 - Extração de Informação/musicas.py
@@ -2,21 +2,20 @@ import json
 import urllib
 import urllib2
 import psycopg2
-import psycopg2.extras
-from bs4 import BeautifulSoup
+import psycopg2.extras 
+import csv
 
-<<<<<<< HEAD
 try:
 	conn = psycopg2.connect("dbname='1802BandoDeDados' user='1802BandoDeDados' host='200.134.10.32' password='803322'")
 except:
 	print "Falha ao se conectar ao banco de dados. Terminando o programa..."
 	end = True
 
+listamusicas = []
 cur = conn.cursor()
 try:
 	cur.execute("SELECT nome_artistico FROM artista_musical")
 	for nomes in cur:
-		print nomes[0]
 		url = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=" + nomes[0] + "&format=json"
 		response = urllib.urlopen(url)
 		data = json.loads(response.read())
@@ -73,7 +72,6 @@ try:
 			nome = nome[nome.find("=")+2:len(nome)]
 			if nome.find("<") != -1:
 				nome = nome[0:nome.find("<")]
-			print nome
 
 			iniciopais = cantor.find("birth_place") + len("birth_place")
 			cantor = cantor[iniciopais:len(cantor)]
@@ -97,39 +95,9 @@ try:
 			else:
 				pais = "NULL"
 			pais = pais[1:len(pais)]
-			print cidade
-			print pais
 
 			if cantor.find("genre") != -1:
 				iniciogenero = cantor.find("genre") + len("genre")
-=======
-url = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=Alestorm&format=json"
-response = urllib.urlopen(url)
-data = json.loads(response.read())
-string = data["query"]["pages"]
-pageid = []
-
-for letra in string:
-	pageid = letra
-
-string2 = data["query"]["pages"][pageid]["revisions"][0]["*"]
-
-indicenomeini = string2.find("name")
-indicenomefim = indicenomeini + len("name")
-
-nome = string2[indicenomefim:len(string2)]
-fimnome = nome.find("\n")
-nome = nome[0:fimnome]
-inicionome = nome.find("=")
-print nome[inicionome+2:fimnome]
-
-if string2.find("origin"):
-	if string2.find("origin") > 0:
-		if string2.find("birth_place"):
-			if string2.find("origin") < string2.find("birth_place"):
-				indicepaisini = string2.find("origin")
-				indicepaisfim = indicepaisini + len("origin")
->>>>>>> 2d11f55eb04ede08423b05a1289d0c713dd1fa05
 			else:
 				iniciogenero = cantor.find("Genre") + len("Genre")	
 			cantor = cantor[iniciogenero:len(cantor)]
@@ -153,7 +121,6 @@ if string2.find("origin"):
 				genero = genero.replace("]","")
 				if genero.find("|") != -1:
 					genero = genero[0:genero.find("|")]
-				print genero
 			else:
 				genero = cantor
 				genero = genero.replace("{","")
@@ -162,56 +129,79 @@ if string2.find("origin"):
 				genero = genero.replace("]","")
 				genero = genero.replace(" = hlist|","")
 				genero = genero[0:genero.find("|")]
-				print genero
+
+			listamusicas.append("https://en.wikipedia.org/wiki/" + nomes[0] + "|" + nome + "|" + cidade + "|" + pais + "|" + genero)
 		else:
-<<<<<<< HEAD
-			print "nao eh pessoa"
+			banda = string2
+			inicionome = banda.find("name") + len("name")
+			banda = banda[inicionome:len(banda)]
+			nome = banda[0:banda.find("\n")]
+			nome = nome[nome.find("=")+2:len(nome)]
+			if nome.find("<") != -1:
+				nome = nome[0:nome.find("<")]
+
+			iniciopais = banda.find("origin") + len("origin")
+			banda = banda[iniciopais:len(banda)]
+			pais = banda[0:banda.find("\n")]
+			pais = pais[pais.find("=")+2:len(pais)]
+			pais = pais.replace("[","")
+			pais = pais.replace("]","")
+			if pais.find("<") != -1:
+				pais = pais[0:pais.find("<")]
+			count = 0
+			for c in pais:
+				if c == ",":
+					count+=1
+			if count == 0:
+				cidade = "NULL"
+				pais = " " + pais
+			elif count == 1:
+				cidade,pais = pais.split(",")
+			elif count == 2:
+				cidade,estado,pais = pais.split(",")
+			else:
+				pais = "NULL"
+			pais = pais[1:len(pais)]
+
+			if banda.find("genre") != -1:
+				iniciogenero = banda.find("genre") + len("genre")
+			else:
+				iniciogenero = banda.find("Genre") + len("Genre")	
+			banda = banda[iniciogenero:len(banda)]
+			genero = banda[0:banda.find("\n")]
+			genero = genero[genero.find("=")+2:len(genero)]
+			if genero.find("<") < genero.find("{") and genero.find("<") != -1:
+				genero = genero[genero.find(">"):len(genero)]
+			elif genero.find("<") > genero.find("{") and genero.find("<") != -1:
+				genero = genero[0:genero.find("<")]
+			genero = genero.replace("{","")
+			genero = genero.replace("}","")
+			genero = genero.replace("[","")
+			genero = genero.replace("]","")
+			genero = genero[genero.find("flatlist|"):len(genero)]
+			genero = genero[0:genero.find("|")]
+			if genero.find("flatlist") != -1:
+				genero = banda
+				genero = genero[genero.find("*")+2:len(genero)]
+				genero = genero[0:genero.find("\n")]
+				genero = genero.replace("[","")
+				genero = genero.replace("]","")
+				if genero.find("|") != -1:
+					genero = genero[0:genero.find("|")]
+			else:
+				genero = banda
+				genero = genero.replace("{","")
+				genero = genero.replace("}","")
+				genero = genero.replace("[","")
+				genero = genero.replace("]","")
+				genero = genero.replace(" = hlist|","")
+				genero = genero[0:genero.find("|")]
+
+			listamusicas.append("https://en.wikipedia.org/wiki/" + nomes[0] + "|" + nome + "|" + cidade + "|" + pais + "|" + genero)
 
 except Exception as e: 
 	print e
 
-
-=======
-			indicepaisini = string2.find("origin")
-			indicepaisfim = indicepaisini + len("origin")
-	else:
-		indicepaisini = string2.find("birth_place")
-		indicepaisfim = indicepaisini + len("birth_place")
-
-origem = string2[indicepaisfim:len(string2)]
-print indicepaisfim
-fimorigem = origem.find("\n")
-origem = origem[0:fimorigem]
-inicioorigem = origem.find("=")
-novoorigem = origem.replace("[", "")
-novonovoorigem = novoorigem.replace("]", "")
-if novonovoorigem.find("<"):
-	fimorigem = novonovoorigem.find(" <")
-	if fimorigem > 0:
-		novonovoorigem = novonovoorigem[0:fimorigem]
-count = 0
-for c in novonovoorigem:
-	if c == ",":
-		count+=1
-print count
-if count == 1:
-	cidade,pais = novonovoorigem.split(",")
-if count == 2:
-	cidade,estado,pais = novonovoorigem.split(",")
-cidade = cidade.replace("=", "")
-countcidade = 0
-for c in cidade:
-	if c != " ":
-		break;
-	countcidade+=1
-cidade = cidade[countcidade:len(cidade)]
-countpais = 0
-for c in pais:
-	if c != " ":
-		break;
-	countpais+=1
-pais = pais[countpais:len(pais)]
-print cidade
-print pais
->>>>>>> 2d11f55eb04ede08423b05a1289d0c713dd1fa05
-
+file = open("musicas.csv",'w')
+for linha in listamusicas:
+	file.write("%s\n" % linha.encode('utf-8'))
