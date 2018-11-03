98c7023ff1799f9eb62a0fee3f8ef71058a5dba8
Giuliana Martins Silva <giusil@alunos.utfpr.edu.br>
2018-11-03 18:56:27 -0300

    Criacao do xml e insercao no banco de dados finalizado

--- 06 - Extração de Informação/movie.py
+++ 06 - Extração de Informação/movie.py
@@ -23,39 +23,52 @@ def listagem():
 
 def atualizar_filme(id_s,nome_s,data_s,categoria_s):
 	update = "UPDATE filmes SET nome = 'nome_s', ano_lancamento = data_s, categoria = 'categoria_s' WHERE id='ttid_s';"
+	nome_s = nome_s.replace("'","")
 	update = update.replace("nome_s", nome_s)
 	update = update.replace("data_s", data_s)
 	update = update.replace("categoria_s", categoria_s)
 	update = update.replace("id_s", id_s)
-	print(update)
 	return update;
 
 def atualizar_categoria(categoria_s):
 	update = "insert into categoria (nome) values ('categoria_s');"
 	update = update.replace("categoria_s", categoria_s)
-	print(update)
 	return update;
 
 def procurar_categoria(categoria_s):
 	search = "select * from categoria where nome = 'categoria_s';"
 	search = search.replace("categoria_s", categoria_s)
-	print(search)
 	return search;
 
+def insere_no_arquivo(arquivo,id_s,nome_s,data_s,categoria_s, diretor_s):
+	dados_filme = '\t<Movie id="id_s" nome = "nome_s" categoria = "categoria_s" ano_lancamento = "data_s" diretor = "diretor_s" />\n'
+	nome_s = nome_s.replace("'","")
+	dados_filme = dados_filme.replace("nome_s", nome_s)
+	dados_filme = dados_filme.replace("data_s", data_s)
+	dados_filme = dados_filme.replace("categoria_s", categoria_s)
+	dados_filme = dados_filme.replace("id_s", id_s)
+	dados_filme = dados_filme.replace("diretor_s", diretor_s)
+	arquivo.write(dados_filme)
+
 #Inicializa a classe Filmes
+arquivo = open("movie.xml","w")
+arquivo.write('<Movies>\n')
+
 ia = IMDb()
 connection.set_client_encoding('LATIN9') #Não sei o que faz, mas coloquei.
-cur = connection.cursor() #Se conecta ao banco.
+cur = connection.cursor() #Se conecta ao banco.s
 try:
 	cur.execute("SELECT id FROM filmes;") #Testa a execução do SQL da função Listagem.
+	cur2 = connection.cursor()
+	cur3 = connection.cursor()
 	for filme in cur:
 		id_filme = filme[0][2:]
 		filmes_imdb= ia.get_movie(id_filme)
 		try:
-			cur.execute(procurar_categoria(filmes_imdb['genres'][0]))
-			verifica = cur.fetchall()
+			cur2.execute(procurar_categoria(filmes_imdb['genres'][0]))
+			verifica = cur2.fetchall()
 			if len(verifica) == 0:
-				cur.execute(atualizar_categoria(filmes_imdb['genres'][0]))
+				cur3.execute(atualizar_categoria(filmes_imdb['genres'][0]))
 				print("Nova insercao da categoria", filmes_imdb['genres'][0])
 			else:
 				print("Categoria", filmes_imdb['genres'][0], "já inserida.")
@@ -63,14 +76,22 @@ try:
 			print("Erro ao procurar categoria.")
 			
 		try:
-			cur.execute(atualizar_filme(id_filme,filmes_imdb['title'],str(filmes_imdb['year']),filmes_imdb['genres'][0]))
+			cur3.execute(atualizar_filme(id_filme,filmes_imdb['title'],str(filmes_imdb['year']),filmes_imdb['genres'][0]))
+			insere_no_arquivo(arquivo,id_filme,filmes_imdb['title'],str(filmes_imdb['year']),filmes_imdb['genres'][0],filmes_imdb['directors'][0]['name'])
+			print("Filme", filmes_imdb['title'], "inserido.")
 		except:
 			print("Erro ao inserir ou atualizar filme", filmes_imdb['title'])
-			continue
+
 except: #Se der falha na execução do SQL
 	print("Falha ao Listar.")
-	connection.commit() #Não sei o que faz mas precisa.
-	cur.close() #Fecha o cur.
+connection.commit() # efetiva as transações
+cur.close() #Fecha o cur.
+
+arquivo.write('</Movies>')
+
+arquivo.close()
+
+
 
 """
 #Teste
