eba009e81b565be5e278fa53aa9bbdba792e94c7
Giuliana Martins Silva <giusil@alunos.utfpr.edu.br>
2018-11-03 16:05:47 -0300

    Falta fazer tratamento de string quando houver apóstrofe no meio do nome do filme

--- 06 - Extração de Informação/teste.py
+++ 06 - Extração de Informação/teste.py
@@ -22,44 +22,55 @@ def listagem():
 	return lista;
 
 def atualizar_filme(id_s,nome_s,data_s,categoria_s):
-	update = 'UPDATE filmes SET nome ="nome_s", ano_lancamento = data_s, categoria = "categoria_s" WHERE id="ttid_s";'
+	update = "UPDATE filmes SET nome = 'nome_s', ano_lancamento = data_s, categoria = 'categoria_s' WHERE id='ttid_s';"
 	update = update.replace("nome_s", nome_s)
 	update = update.replace("data_s", data_s)
 	update = update.replace("categoria_s", categoria_s)
 	update = update.replace("id_s", id_s)
+	print(update)
 	return update;
 
 def atualizar_categoria(categoria_s):
 	update = "insert into categoria (nome) values ('categoria_s');"
 	update = update.replace("categoria_s", categoria_s)
+	print(update)
 	return update;
+
+def procurar_categoria(categoria_s):
+	search = "select * from categoria where nome = 'categoria_s';"
+	search = search.replace("categoria_s", categoria_s)
+	print(search)
+	return search;
 
 #Inicializa a classe Filmes
 ia = IMDb()
 connection.set_client_encoding('LATIN9') #Não sei o que faz, mas coloquei.
 cur = connection.cursor() #Se conecta ao banco.
-cur.execute("SELECT id FROM filmes;") #Testa a execução do SQL da função Listagem.
-for filme in cur:
-	cur1 = connection.cursor()
-
-	id_filme = filme[0][2:]
-	filmes_imdb = ia.get_movie(id_filme)
-	procura_categoria = "select nome from categoria where nome = 'categoria_s';"
-	procura_categoria = procura_categoria.replace("categoria_s", filmes_imdb['genres'][0])
-	print(procura_categoria)
-	cur1.execute(procura_categoria)
-	achou = cur1.fetchall()
-	print(achou)
-	if len(achou) == 0:
-		cur1.execute(atualizar_categoria(filmes_imdb['genres'][0]))
-	else:
-		print("categoria já inserida")
-	
-	cur1.execute(atualizar_filme(id_filme,filmes_imdb['title'],str(filmes_imdb['year']),filmes_imdb['genres'][0]))
-	print("filme já inserido")
-print("Falha ao Listar.")
-connection.commit() #Não sei o que faz mas precisa.
-cur.close() #Fecha o cur.
+try:
+	cur.execute("SELECT id FROM filmes;") #Testa a execução do SQL da função Listagem.
+	for filme in cur:
+		id_filme = filme[0][2:]
+		filmes_imdb= ia.get_movie(id_filme)
+		try:
+			cur.execute(procurar_categoria(filmes_imdb['genres'][0]))
+			verifica = cur.fetchall()
+			if len(verifica) == 0:
+				cur.execute(atualizar_categoria(filmes_imdb['genres'][0]))
+				print("Nova insercao da categoria", filmes_imdb['genres'][0])
+			else:
+				print("Categoria", filmes_imdb['genres'][0], "já inserida.")
+		except:
+			print("Erro ao procurar categoria.")
+			
+		try:
+			cur.execute(atualizar_filme(id_filme,filmes_imdb['title'],str(filmes_imdb['year']),filmes_imdb['genres'][0]))
+		except:
+			print("Erro ao inserir ou atualizar filme", filmes_imdb['title'])
+			continue
+except: #Se der falha na execução do SQL
+	print("Falha ao Listar.")
+	connection.commit() #Não sei o que faz mas precisa.
+	cur.close() #Fecha o cur.
 
 """
 #Teste
