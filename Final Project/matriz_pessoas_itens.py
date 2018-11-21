492e4c78864ec0811523bdf25ad0648e96a356c4
Giuliana Martins Silva <giusil@alunos.utfpr.edu.br>
2018-11-21 01:30:53 -0200

    Implementada funções de matriz de usuarios por artistas

---
+++ Final Project/matriz_pessoas_itens.py
@@ -0,0 +1,55 @@
+import psycopg2;
+import numpy as np;
+
+#Conecta ao banco de dados
+try:
+	conn = psycopg2.connect("dbname='1802BandoDeDados' user='1802BandoDeDados' host='200.134.10.32' password='803322'")
+except:
+	print("Falha ao se conectar ao banco de dados.")
+
+#Obtem o numero de artistas para a criacao da matriz
+def obtemNumeroDeArtistas(conn):
+	cur = conn.cursor()
+	consulta = str("select count(*) from artista_musical;")
+	cur.execute(consulta)
+	num_artista = cur.fetchone()
+	cur.close()
+	return num_artista[0];
+
+#Obtem o numero de usuarios para a criacao da matriz
+def obtemNumeroDeUsuarios(conn):
+	cur = conn.cursor()
+	consulta = str("select count(*) from pessoa;")
+	cur.execute(consulta)
+	num_usuario = cur.fetchone()
+	cur.close()
+	return num_usuario[0];
+
+#Essa view sera usada para fazer a associacao de um artista a um numero
+def criaViewNumArtista(conn):
+	cur = conn.cursor()
+	consulta = str("create or replace view num_artista as select row_number() over(order by artista_musical) as num, id from artista_musical;");
+	cur.execute(consulta)
+	cur.close()
+
+#Essa view sera usada para fazer a associacao de um usuario a um numero
+def criaViewNumUsuario(conn):
+	cur = conn.cursor()
+	consulta = str("create or replace view num_usuario as select row_number() over(order by pessoa) as num, login from pessoa;");
+	cur.execute(consulta)
+	cur.close()
+
+#Cria Matriz UsuarioArtista
+def criaMatrizUsuarioArtista(conn):
+	usuario_artista = np.zeros((obtemNumeroDeUsuarios(conn),obtemNumeroDeArtistas(conn)))
+	return usuario_artista;
+
+def preencheMatrizUsuarioArtista(conn, num_usuario, usuario_artista):
+    cur = conn.cursor()
+    for user in range(1,num_usuario):
+        consulta = str("select a.num as num_artista, l.nota as nota from like_artista l, num_usuario u, num_artista a where u.num = user and u.login = l.login and l.id = a.id;")
+        consulta = consulta.replace("user", str(user))
+        cur.execute(consulta)
+        notas = cur.fetchall()
+        for i in notas:
+            usuario_artista[user][i[0]] = i[1]
