9ae57ebf80df8ed08a73ff13867de376bb5cd384
Alexandre Herrero Matias <alexandre@localhost.localdomain>
2018-11-29 13:42:00 -0200

    adicionado kmeans.py

--- Final Project/kmeans.py
+++ Final Project/kmeans.py
@@ -2,6 +2,7 @@ import psycopg2
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from itertools import groupby
 
 try:
 	conn = psycopg2.connect("dbname='1802BandoDeDados' user='1802BandoDeDados' host='200.134.10.32' password='803322'")
@@ -83,6 +84,7 @@ try:
 except Exception as e:
 	print(e)
 
+loginsgrupos = []
 fig, ax = plt.subplots()
 ax.set_xlim(-6,6)
 ax.set_ylim(-6,6)
@@ -116,16 +118,24 @@ while pos <= 45:
 		ax.text(x[pos] - 0.2, y[pos] + 0.25, str(num + 1), fontsize=6)
 		listax.append(x[pos])
 		listay.append(y[pos])
-		print(logins[pos] + " - " + str(num + 1))
+		loginsgrupos.append(logins[pos])
 		num += 1
 	pos += 1
 
 X = np.column_stack((listax, listay))
-print(X)
+
+Pessoas1 = np.column_stack((x, y, logins))
+print(Pessoas1)
+
+Pessoas = np.column_stack((listax, listay, loginsgrupos))
+print(Pessoas)
 
 kmeans = KMeans(n_clusters=4, random_state=0).fit(X)
-print(kmeans.labels_)
 
+grupo1 = []
+grupo2 = []
+grupo3 = []
+grupo4 = []
 fig, ax2 = plt.subplots()
 ax2.set_xlim(-6,6)
 ax2.set_ylim(-6,6)
@@ -136,13 +146,47 @@ i = 0
 for indice in kmeans.labels_:
 	if indice == 0:
 		ax2.plot(listax[i], listay[i], 'ro', color='#0000FF')
+		grupo1.append(Pessoas[i][2])
 	if indice == 1:
 		ax2.plot(listax[i], listay[i], 'ro', color='#FF0000')
+		grupo2.append(Pessoas[i][2])
 	if indice == 2:
 		ax2.plot(listax[i], listay[i], 'ro', color='#F9D307')
+		grupo3.append(Pessoas[i][2])
 	if indice == 3:
 		ax2.plot(listax[i], listay[i], 'ro', color='#00FF00')
+		grupo4.append(Pessoas[i][2])
 	i += 1
 ax2.grid(True, which='both')
+
+nomesgrupo1 = []
+nomesgrupo2 = []
+nomesgrupo3 = []
+nomesgrupo4 = []
+for nome1 in grupo1:
+	cur.execute("SELECT nome_completo FROM pessoa WHERE login = '" + nome1 +"'")
+	for nome in cur:
+		nomesgrupo1.append(nome[0])
+for nome2 in grupo2:
+	cur.execute("SELECT nome_completo FROM pessoa WHERE login = '" + nome2 +"'")
+	for nome in cur:
+		nomesgrupo2.append(nome[0])
+for nome3 in grupo3:
+	cur.execute("SELECT nome_completo FROM pessoa WHERE login = '" + nome3 +"'")
+	for nome in cur:
+		nomesgrupo3.append(nome[0])
+for nome4 in grupo4:
+	cur.execute("SELECT nome_completo FROM pessoa WHERE login = '" + nome4 +"'")
+	for nome in cur:
+		nomesgrupo4.append(nome[0])
+
+print("Grupo 1:")
+print(nomesgrupo1)
+print("Grupo 2:")
+print(nomesgrupo2)
+print("Grupo 3:")
+print(nomesgrupo3)
+print("Grupo 4:")
+print(nomesgrupo4)
 
 plt.show()
